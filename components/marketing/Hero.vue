f1165601ad2c42139974eb80edd3ddb27b675548
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-12 14:30:18 -0500

    Add marketing hero component

---
+++ components/marketing/Hero.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section>
+    <div class="max-w-screen-xl mx-auto px-4 py-28 gap-12 md:px-8">
+      <div class="space-y-5 max-w-4xl mx-auto text-center">
+        <h1 class="text-sm text-primary-600 font-medium">
+          Take control of your Kitchen
+        </h1>
+        <h2 class="text-4xl font-extrabold mx-auto md:text-5xl">
+          With <span class="text-transparent bg-clip-text bg-gradient-to-r from-[$4ade80] to-[#166534]">Kitchen
+            Butler</span>
+        </h2>
+        <p class="max-w-2xl mx-auto">
+          Always know exactly what's in your kitchen, reducing over purchasing or running out unexpectedly.
+        </p>
+        <div class="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
+          <NuxtLink href="#pricing"
+            class="block py-2 px-4 text-white font-medium bg-primary-600 duration-150 hover:bg-primary-500 active:bg-primary-700 rounded-lg shadow-lg hover:shadow-none">
+            Get Started
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="mt-14">
+        <img src="~/assets/images/hero.png" class="w-full shadow-lg rounded-lg border dark:invert" alt="">
+      </div>
+    </div>
+  </section>
+</template>
