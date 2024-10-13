9e4ee16352c79a50055d886b287252e60e653896
Vincenzo Fehring <vinfehring@gmail.com>
2024-10-12 19:43:17 -0500

    Hook up login logic

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
 import type { FormSubmitEvent } from '#ui/types'
+import { z } from 'zod'
+
+const user = useUser()
+if (user.value)
+  await navigateTo('/app')
+
+const schema = z.object({
+  email: z.string().email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters'),
+})
+
+type Schema = z.output<typeof schema>
 
 const errorMessage = ref('')
 const state = reactive({
@@ -8,7 +20,16 @@ const state = reactive({
 })
 
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-
+  const res = await $fetch('/api/auth/login', {
+    ignoreResponseError: true,
+    method: 'POST',
+    body: event.data,
+    redirect: 'manual',
+  })
+  if (res.success)
+    await navigateTo('/app')
+  else
+    errorMessage.value = res.message
 }
 </script>
 
